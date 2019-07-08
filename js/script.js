5cbf6d4ba06ce9c997555004356e9f1f59de6832
Veget-ables <makuhari109@gmail.com>
2019-07-08 12:59:29 +0900

    [refactoring] script.js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,53 @@
-chrome.extension.onMessage.addListener(function(menuItem) {
-  chrome.storage.sync.get(null, function(items) {
-	  // 現在フォーカスが与えられている要素を取得する
-	 	const active_element = document.activeElement;
+const getTomorrowDate = () => {
+    const today = moment();
+    if (today.day() === 5) {
+        return today.add(3, 'd');
+    } else {
+        return today.add(1, 'd');
+    }
+};
 
-		 // フォーカスしている部分を取得
-		const target_area = active_element.id;
-		const target = document.getElementById(target_area);
+chrome.extension.onMessage.addListener(function (menuItem) {
+    chrome.storage.sync.get(null, async function (items) {
+        // 現在フォーカスが与えられている要素を取得する
+        const target = document.activeElement;
+        if (target === null) return;
 
-		// フォーカスしていなかったら処理をやめる
-		if (target_area === '') return;
-
-		// 日付処理
-		let date;
-		const today = moment();
-		let tomorrow;
-		// todayが金曜なら、tomorrowを翌週の月曜にする
-		if (moment().day() === 5) tomorrow = moment(today).add(3, 'd');
-		else tomorrow = moment(today).add(1, 'd');
-		// オプション画面の日付情報を取得
-		const selected_date = items.select;
-		//date変数にそれぞれの場合の日付を代入
-		switch (menuItem.id) {
-			case TODAY_ID:
-			case TEMPLATE_ID:
-				date = today;
-				break;
-			case TOMORROW_ID:
-				date = tomorrow;
-				break;
-			default:
-				date = moment(selected_date);
-				break;
-		}
-
-		//RESTで取得したJSON形式のスケジュール情報をもとに処理を行う
-		getSchedule(date).done(function(json) {
-			const schedule = formatSchedule($(json));
-			// オプション画面の非公開予定の値
-			const type = items.secret;
-			const form = items.form;
-
-			// スケジュールの挿入部分
-			switch (menuItem.id) {
-				case TODAY_ID:
-					target.innerHTML += makehtml(schedule, type, today.format('YYYY-MM-DD'));
-					break;
-				case TOMORROW_ID:
-					target.innerHTML += makehtml(schedule, type, tomorrow.format('YYYY-MM-DD'));
-					break;
-				case TEMPLATE_ID:
-					getSchedule(tomorrow).done(function(tomorrow_json) {
-						const tomorrow_schedule = formatSchedule($(tomorrow_json));
-						target.innerHTML += makeForm(form, schedule, tomorrow_schedule, type, today, tomorrow);
-					});
-					break;
-				default:
-					target.innerHTML += makehtml(schedule, type, selected_date);
-					break;
-			}
-			
-		});
-	});
+        switch (menuItem.id) {
+            case MYSELF_ID: {
+                const today       = moment();
+                const json        = await getSchedule(today);
+                const schedule    = formatSchedule($(json));
+                target.innerHTML += makehtml(schedule, items.secret, today.format('YYYY-MM-DD'));
+                break;
+            }
+            case TOMORROW_ID: {
+                const tomorrow    = getTomorrowDate();
+                const json        = await getSchedule(tomorrow);
+                const schedule    = formatSchedule($(json));
+                target.innerHTML += makehtml(schedule, items.secret, tomorrow.format('YYYY-MM-DD'));
+                break;
+            }
+            case TEMPLATE_ID: {
+                const today               = moment();
+                const tomorrow            = getTomorrowDate();
+                const todayJson           = await getSchedule(today);
+                const tomorrowJson        = await getSchedule(tomorrow);
+                const todaySchedule       = formatSchedule($(todayJson));
+                const tomorrowSchedule    = formatSchedule($(tomorrowJson));
+                target.innerHTML += makeForm(items.form, todaySchedule, tomorrowSchedule, items.secret, today, tomorrow);
+                break;
+            }
+            case SELECT_ID: {
+                const selectedDate    = moment(items.select); // オプション画面の日付情報を取得
+                const json            = await getSchedule(selectedDate);
+                const schedule        = formatSchedule($(json));
+                target.innerHTML += makehtml(schedule, items.secret, items.select);
+                break;
+            }
+            default:
+                // TODO myGroupの処理
+                break;
+        }
+    });
 });

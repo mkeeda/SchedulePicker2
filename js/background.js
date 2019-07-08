c34e56ff2f6afae6ff84e64f3155e6d45e0eb8f3
Veget-ables <makuhari109@gmail.com>
2019-07-08 11:53:29 +0900

    backgroud.jsから作成したMyGroup取得APIを呼び出すようにした

--- js/background.js
+++ js/background.js
@@ -6,15 +6,15 @@ const ROOT_ID        = "root";
 const TODAY_ID       = "today";
 
 const DEFAULT_MENU_ITEMS = [
-    {[M_ID]: ROOT_ID,    [M_TITLE]: "SchedulePicker"},
-    {[M_ID]: TODAY_ID,   [M_TITLE]: "Today",    [M_PARENT_ID]: ROOT_ID},
-    {[M_ID]: "form",     [M_TITLE]: "Template", [M_PARENT_ID]: ROOT_ID},
+    {[M_ID]: ROOT_ID, [M_TITLE]: "SchedulePicker"},
+    {[M_ID]: TODAY_ID, [M_TITLE]: "Today", [M_PARENT_ID]: ROOT_ID},
+    {[M_ID]: "form", [M_TITLE]: "Template", [M_PARENT_ID]: ROOT_ID},
     {[M_ID]: "tomorrow", [M_TITLE]: "Tomorrow", [M_PARENT_ID]: ROOT_ID},
-    {[M_ID]: "select",   [M_TITLE]: "Select",   [M_PARENT_ID]: ROOT_ID},
-    {[M_ID]: "myself",   [M_TITLE]: "自分の予定", [M_PARENT_ID]: TODAY_ID}
+    {[M_ID]: "select", [M_TITLE]: "Select", [M_PARENT_ID]: ROOT_ID},
+    {[M_ID]: "myself", [M_TITLE]: "自分の予定", [M_PARENT_ID]: TODAY_ID}
 ];
 
-const addMenuItem = function (item) {
+const addMenuItem = (item) => {
     chrome.contextMenus.create({
         [M_ID]           : item[M_ID],
         [M_TITLE]        : item[M_TITLE],
@@ -25,22 +25,19 @@ const addMenuItem = function (item) {
     });
 };
 
-const addMenuItems = function (items) {
+const addMenuItems = (items) => {
     items.map(item => addMenuItem(item))
+};
+
+// Myグループ一覧を取得し、親Menuにぶら下げる
+const setupContextMenus = async () => {
+    const myGroups = await getMyGroups();
+    const groupMenuItems = myGroups.map(g => {
+        return {[M_ID]: g.key, [M_TITLE]: g.name, [M_PARENT_ID]: TODAY_ID}
+    });
+    addMenuItems(groupMenuItems);
 };
 
 // 右クリックでデフォルト表示されるMenu一覧を作成
 addMenuItems(DEFAULT_MENU_ITEMS);
-
-// Myグループ一覧を取得し、親Menuにぶら下げる
-const garoon = new GaroonSoap('https://bozuman.cybozu.com/g/');
-garoon.base.getMyGroupVersions([]).then(events => {
-        const ids = events.map(event => event.id);
-        garoon.base.getMyGroupsById(ids).then(groups => {
-            const groupMenuItems = groups.map(g => {
-                return {[M_ID]: g.key, [M_TITLE]: g.name, [M_PARENT_ID]: TODAY_ID}
-            });
-            addMenuItems(groupMenuItems)
-        });
-    }
-);
+setupContextMenus();

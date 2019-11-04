bedb2ffddac4322181692903ee7688460639496d
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-04 20:57:48 +0900

    GaroonSoapでガルーンのスケジュールを取得できるようにする

--- src/content/contentscript.ts
+++ src/content/contentscript.ts
@@ -1,6 +1,11 @@
-const hoge = 'hoge';
-console.log(hoge);
-
-if (hoge) {
-    console.log(hoge);
-}
+import GaroonSoap from 'garoon-soap';
+console.log('1');
+const garoon = new GaroonSoap(`https://bozuman.s.cybozu.com/g/`);
+console.log('hoge');
+const start = new Date();
+start.setDate(start.getDate() + 2);
+const end = new Date();
+end.setDate(start.getDate() + 3);
+garoon.schedule.getEvents(start, end).then(events => {
+    console.log(events);
+});

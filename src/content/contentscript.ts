0589229ed692a07bb602e73d4bf5c80d3a0d6257
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-12 23:38:28 +0900

    要素からフォーカスが外れているときはreturnするようにした

--- src/content/contentscript.ts
+++ src/content/contentscript.ts
@@ -1,8 +1,13 @@
 import { StorageKeys } from '../background/eventtype';
 
-chrome.runtime.onMessage.addListener((schedule: any) => {
+chrome.runtime.onMessage.addListener((events: any) => {
     chrome.storage.sync.get([StorageKeys.IS_INCLUDE, StorageKeys.DATE, StorageKeys.TEMPLATE_TEXT], item => {
-        console.log('cs');
-        console.log(schedule);
+        console.log(events);
+        // 現在フォーカスが与えられている要素を取得する
+        const target = document.activeElement;
+        // フォーカスが外れているときactiveElementはnullかbodyを返す
+        if (target === null || target.tagName === 'BODY') {
+            return;
+        }
     });
 });

68d986eb1238afaf2079bc62acc7a33f3908560f
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-13 00:04:36 +0900

    今日の予定の「自分」とマイグループの予定を選択すると、入力中の要素にガルーンの予定が挿入されるようにした

--- src/content/contentscript.ts
+++ src/content/contentscript.ts
@@ -1,13 +1,30 @@
 import { StorageKeys } from '../background/eventtype';
+import { formatDate } from '../background/util';
+
+// FIXME: 変数名とか内部の処理とかmakehtml.jsを見ながら修正必要
+const createHtml = (events: any): string => {
+    const title = `<div>【date】の予定</div>`;
+    const body = events
+        .map(event => {
+            return `
+            <div>
+                <span>${formatDate(new Date(event.start.dateTime))}-${formatDate(new Date(event.end.dateTime))}</span>
+                <a href="https://bozuman.cybozu.com/g/schedule/view.csp?event=${event.id}">${event.subject}</a>
+            </div>
+            `;
+        })
+        .join('');
+    return `${title}${body}`;
+};
 
 chrome.runtime.onMessage.addListener((events: any) => {
     chrome.storage.sync.get([StorageKeys.IS_INCLUDE, StorageKeys.DATE, StorageKeys.TEMPLATE_TEXT], item => {
-        console.log(events);
         // 現在フォーカスが与えられている要素を取得する
         const target = document.activeElement;
         // フォーカスが外れているときactiveElementはnullかbodyを返す
         if (target === null || target.tagName === 'BODY') {
             return;
         }
+        document.execCommand('insertHtml', false, createHtml(events));
     });
 });

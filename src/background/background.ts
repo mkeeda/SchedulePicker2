85b9a3d31289f0c29f90c124648c9e930a3da21c
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-13 23:59:38 +0900

    APIの接続に失敗したときはcontent_script側にEventsType.Errorを渡す

--- src/background/background.ts
+++ src/background/background.ts
@@ -36,28 +36,35 @@ const setupContextMenu = async (domain: string): Promise<void> => {
 
     chrome.contextMenus.onClicked.addListener((info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
         chrome.storage.sync.get([StorageKeys.IS_INCLUDE, StorageKeys.DATE, StorageKeys.TEMPLATE_TEXT], async items => {
-            switch (info.menuItemId) {
-                case ContextMenuIds.MYSELF: {
-                    const eventInfoList = await logic.getSortedMyEvents(ScheduleEventType.TODAY);
-                    chrome.tabs.sendMessage(tab!.id!, { eventType: EventsType.MY_EVENTS, events: eventInfoList });
-                    break;
+            try {
+                switch (info.menuItemId) {
+                    case ContextMenuIds.MYSELF: {
+                        const eventInfoList = await logic.getSortedMyEvents(ScheduleEventType.TODAY);
+                        chrome.tabs.sendMessage(tab!.id!, { eventType: EventsType.MY_EVENTS, events: eventInfoList });
+                        break;
+                    }
+                    case ContextMenuIds.NEXT_BUSINESS_DAY: {
+                        chrome.tabs.sendMessage(tab!.id!, ContextMenuIds.NEXT_BUSINESS_DAY);
+                        break;
+                    }
+                    case ContextMenuIds.TEMPLATE: {
+                        chrome.tabs.sendMessage(tab!.id!, ContextMenuIds.TEMPLATE);
+                        break;
+                    }
+                    default: {
+                        const myGroupEventList = await logic.getMyGroupSchedule(
+                            ScheduleEventType.TODAY,
+                            info.menuItemId
+                        );
+                        chrome.tabs.sendMessage(tab!.id!, {
+                            eventType: EventsType.MY_GROUP_EVENT,
+                            events: myGroupEventList,
+                        });
+                        break;
+                    }
                 }
-                case ContextMenuIds.NEXT_BUSINESS_DAY: {
-                    chrome.tabs.sendMessage(tab!.id!, ContextMenuIds.NEXT_BUSINESS_DAY);
-                    break;
-                }
-                case ContextMenuIds.TEMPLATE: {
-                    chrome.tabs.sendMessage(tab!.id!, ContextMenuIds.TEMPLATE);
-                    break;
-                }
-                default: {
-                    const myGroupEventList = await logic.getMyGroupSchedule(ScheduleEventType.TODAY, info.menuItemId);
-                    chrome.tabs.sendMessage(tab!.id!, {
-                        eventType: EventsType.MY_GROUP_EVENT,
-                        events: myGroupEventList,
-                    });
-                    break;
-                }
+            } catch (e) {
+                chrome.tabs.sendMessage(tab!.id!, { eventType: EventsType.ERROR, events: [] });
             }
         });
     });

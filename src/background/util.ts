51bba11e300c435e0f48ba60d429b79f743a0d14
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-15 12:06:39 +0900

    時間sortのメソッドを１行で書き直した

--- src/background/util.ts
+++ src/background/util.ts
@@ -1,13 +1,5 @@
 import { EventInfo } from '../types/event';
 
 export const sortByTimeFunc = (eventInfo: EventInfo, nextEventInfo: EventInfo): number => {
-    if (eventInfo.startTime > nextEventInfo.startTime) {
-        return 1; // nextEvent, event の順番に並べ替える
-    }
-
-    if (eventInfo.startTime < nextEventInfo.startTime) {
-        return -1; // event, nextEvent の順番に並べ替える
-    }
-
-    return 0;
+    return eventInfo.startTime.getTime() > nextEventInfo.startTime.getTime() ? 1 : -1;
 };

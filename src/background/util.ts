a527b9d8fe4e8f66cdf9ab22ffd7fcb4ddae2495
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-15 10:25:11 +0900

    backgroundのformatDate関数を使っていなかったので削除

--- src/background/util.ts
+++ src/background/util.ts
@@ -1,12 +1,5 @@
 import { EventInfo } from '../types/event';
 
-export const formatDate = (date: Date): string => {
-    const hour = `0${date.getHours()}`.slice(-2);
-    const minute = `0${date.getMinutes()}`.slice(-2);
-    const time = hour + ':' + minute;
-    return time; // HH:mm
-};
-
 export const sortByTimeFunc = (eventInfo: EventInfo, nextEventInfo: EventInfo): number => {
     if (eventInfo.startTime > nextEventInfo.startTime) {
         return 1; // nextEvent, event の順番に並べ替える

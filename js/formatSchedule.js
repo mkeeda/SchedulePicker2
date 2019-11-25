7233b007fb34f84530046d7c5ac5d05df117f407
Atsusuke <atsusuke-murata@cybozu.co.jp>
2019-11-25 16:47:53 +0900

    不要なファイルを削除

--- js/formatSchedule.js
+++
@@ -1,7 +0,0 @@
-const formatSchedule = function(json) {
-  const schedule = json[0]["events"];
-  schedule.forEach(function(data) {
-    checkScheduleType(data);
-  });
-  return sortTime(schedule);
-};
